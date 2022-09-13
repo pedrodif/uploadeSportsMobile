861a554f0715909bf12ea4fcfe85d8219c8ce701
Pedro Ferreira <pedrodif@icloud.com>
2022-09-13 16:37:02 -0300

    feat: :art: importing fonts, creation of new screens folder and files

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
+// Packages
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black } from '@expo-google-fonts/inter'
+
+// Components
 import { Background } from './src/components/Background';
 import { StatusBar } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
+
+  // render
   return (
     <Background>
       <StatusBar
