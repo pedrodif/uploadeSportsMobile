cb054b20c561119c30954955246cc72ea7096631
Pedro Ferreira <pedrodif@icloud.com>
2022-09-13 19:42:22 -0300

    feat: :art: add game card component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,10 @@ import { styles } from './styles';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+
+// Utils
+import { GAMES } from '../../utils/games'
 
 export function Home() {
   return (
@@ -16,6 +20,10 @@ export function Home() {
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <GameCard
+        data={GAMES[0]}
       />
 
     </View>
