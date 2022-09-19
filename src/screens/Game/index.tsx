b9b2e90151a2201384c623e00ea428d13e9b613a
Pedro Ferreira <pedrodif@icloud.com>
2022-09-19 18:41:13 -0300

    feat: :art: improving definition types and game screen

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,11 +1,20 @@
+// Packages
+import { useRoute } from '@react-navigation/native';
+
 // Components
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Background } from '../../components/Background';
 
+// Types
+import { GameParams } from '../../@types/navigation';
 
+// Styles
 import { styles } from './styles';
 
 export function Game() {
+  const route = useRoute();
+  const game = route.params as GameParams;
+
   return (
     <Background>
       <SafeAreaView style={styles.container}>
