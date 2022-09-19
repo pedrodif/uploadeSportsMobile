86df3b92a4cacf630b1f79f88a0f2c5f8989bbef
Pedro Ferreira <pedrodif@icloud.com>
2022-09-19 15:17:35 -0300

    feat: :art: Problems solving

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -16,7 +16,7 @@ export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([])
 
   useEffect(() => {
-    fetch('http://192.168.90.2:3333/games')
+    fetch('http://192.168.0.34:3333/games')
     .then(response => response.json())
     .then(data => setGames(data))
   }, [])
